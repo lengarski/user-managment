0b388b0e3f36fcde6278056505ebe234ddb6f14b
lengarski <lengarski@gmail.com>
2021-08-17 19:02:21 +0300

    fix db

--- UserManagmetnSystemBE/src/main/java/eu/lengarski/usermanagment/UserManagmentSystemConstants.java
+++ UserManagmetnSystemBE/src/main/java/eu/lengarski/usermanagment/UserManagmentSystemConstants.java
@@ -1,7 +1,5 @@
 package eu.lengarski.usermanagment;
 
-import org.springframework.beans.factory.annotation.Value;
-
 /**
  * Created by Georgi Lengarski on  Thursday 12/08/2021   08:08
  */
@@ -11,5 +9,5 @@ public class UserManagmentSystemConstants
     public static final String DB_URL_SUFIX = ":3306/usermanagment?serverTimezone=UTC";
 
     public static final String DB_USERNAME = "ums";
-    public static final String DB_PASSWORD = "umsumsums";
+    public static final String DB_PASSWORD = "ums";
 }
